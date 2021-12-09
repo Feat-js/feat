a058dc3c25f6423de919700287d7b71952f528c5
serakoi <admin@seer-software.com>
2021-12-09 12:43:00 +0100

    Add comp

---
+++ components/href.js
@@ -0,0 +1,29 @@
+module.exports = {
+    name: "Href",
+    FeatAttributesSupport: false,
+    description: "Redirect a user",
+    selfClosing: true,
+    attributes: [
+        {
+            name: "target",
+            type: "string",
+            description: "Target String", 
+            required: false
+        },   
+        {
+            name: "href",
+            type: "string",
+            description: "Url where user is redirected to", 
+            required: true
+        },        
+        {
+            name: "text",
+            type: "string",
+            description: "Placeholder text", 
+            required: true
+        },
+    ],
+    code: (attributes, content, data, blb) => {
+        return `<a ${attributes.target ? "target=\"" + attributes.target + "\"" : ""} href="${attributes.href ? attributes.href : "#"}">${attributes.text ? attributes.text : "Text undefined."}</a>`;
+    }
+}   
