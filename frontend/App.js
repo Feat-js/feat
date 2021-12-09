1fe145a61700abefbecf9309b521f2e324a673d3
serakoi <admin@seer-software.com>
2021-12-09 13:37:09 +0100

    Feat Jqeury

--- frontend/App.js
+++ frontend/App.js
@@ -1,7 +1,7 @@
 class FeatApp {
     constructor(){
         this.log = (...contents) => console.log(`[FeatApp]`, contents.join(" "));
-        this.log(`Working...`);
+        this.log(`Feat attached!`);
         this.states = {};
     }
 
@@ -21,5 +21,28 @@ class FeatApp {
     getState(name){
         return this.states[name];
     }
+
+    /**
+     * 
+     * @param {String} selector Element selector. Ex: .className #elementID
+     * @returns Element Object
+     */
+    getElement(selector){
+        let el = document.querySelector(selector);
+        return el;
+    }
+
+    /**
+     * 
+     * @param {String} selector Element selector. Ex: .className #elementID
+     * @param {*} className Element Object
+     * @returns 
+     */
+    toggleClass(selector, className){
+        let el = this.getElement(selector);
+        el.classList.toggle(className);
+        return el;
+    }
 }
+// Initiate Feat.
 const Feat = new FeatApp();
